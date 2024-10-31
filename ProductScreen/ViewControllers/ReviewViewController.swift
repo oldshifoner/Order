272c75a5d7778bb997f5d20a59990e423780d9b3
oldshifoner <oldshifoner@gmail.com>
2024-10-31 20:36:35 +0700

    Add class ReviewViewController

---
+++ ProductScreen/ViewControllers/ReviewViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReviewViewController.swift
+//  ProductScreen
+//
+//  Created by Максим Игоревич on 31.10.2024.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
