e1b23c0ae2f04d40323a1b1af82f7712df5c2a67
oldshifoner <oldshifoner@gmail.com>
2024-10-31 21:02:45 +0700

    Add viewModel as ReviewsListViewModel

--- ProductScreen/ViewControllers/ListReviewsViewController.swift
+++ ProductScreen/ViewControllers/ListReviewsViewController.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
+struct MyReviewsList {
+
+    struct Review {
+        let title: String
+        let size: String
+        let evaluation: String? = nil
+        let titleImage: String
+        let selfImages: [String]? = nil
+        let advantages: String? = nil
+        let disadvantages: String? = nil
+        let comment: String? = nil
+        let anonymously: Bool
+        let isWritten: Bool
+    }
+    var screenTitle: String
+    var reviews: [MyReviewsList.Review]
+}
+
+let reviewList = MyReviewsList(screenTitle: "Напишите отзыв", reviews: [
+    .init(title: "Золотое плоское обручальное кольцо 4 мм", size: "17", titleImage: "goldRingPruduct1", anonymously: true, isWritten: false),
+    .init(title: "Золотое плоское обручальное кольцо 4 мм", size: "15", titleImage: "goldRingPruduct2", anonymously: true, isWritten: false),
+    .init(title: "Золотое плоское обручальное кольцо 4 мм", size: "12", titleImage: "goldRingPruduct3", anonymously: true, isWritten: false),
+    .init(title: "Золотое плоское обручальное кольцо 4 мм", size: "20", titleImage: "goldRingPruduct4", anonymously: true, isWritten: false)])
+
+
 class ListReviewsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
 }
---
+++ ProductScreen/ViewModels/ReviewsListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ReviewsListViewModel.swift
+//  ProductScreen
+//
+//  Created by Максим Игоревич on 31.10.2024.
+//
+
+import Foundation
+
+class ReviewsListViewModel {
+    
+}
