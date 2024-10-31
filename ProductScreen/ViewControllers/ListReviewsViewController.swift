8d1aa703bed78f9153c843d95eab00121d683715
oldshifoner <oldshifoner@gmail.com>
2024-10-31 15:25:36 +0700

    Add ListReviewsViewController

---
+++ ProductScreen/ViewControllers/ListReviewsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ListReviewsViewController.swift
+//  ProductScreen
+//
+//  Created by Максим Игоревич on 31.10.2024.
+//
+
+import UIKit
+
+class ListReviewsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
