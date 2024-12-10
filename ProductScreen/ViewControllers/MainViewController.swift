3cb3acf23a1659be5f6535ae6420942644e1ae00
oldshifoner <oldshifoner@gmail.com>
2024-12-10 13:06:24 +0700

    create mainViewController

---
+++ ProductScreen/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  ProductScreen
+//
+//  Created by Максим Игоревич on 10.12.2024.
+//
+
+import UIKit
+
+
+class MainViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        //init
+    }
+}
